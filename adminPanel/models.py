2c9cbce1b3ac4105389673bca2f504c9dac26f41
Carlos Abarca <soporte@compuelec.cl>
2023-06-10 15:22:25 -0400

    conexion y modelo de base de datos para mysql

--- adminPanel/models.py
+++ adminPanel/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    TIPO_USUARIO_CHOICES = [
+        ('admin', 'Administrador'),
+        ('normal', 'Normal'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    correo = models.EmailField(unique=True)
+    tipo = models.CharField(max_length=10, choices=TIPO_USUARIO_CHOICES, default='normal')
+    imagen = models.ImageField(upload_to='usuarios', null=True, blank=True)
+    token = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+
+class Producto(models.Model):
+    codigo = models.CharField(max_length=255, unique=True)
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='productos', null=True, blank=True)
+    stock = models.PositiveIntegerField()
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
